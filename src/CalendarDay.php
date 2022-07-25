6813cfaf2d63f346513fc66fd07d0390fdd6b0bf
Bartosz Bujak <bartosz.bujak@me.com>
2022-07-25 11:34:12 +0200

    Fixed withEvents function for Month view

--- src/CalendarDay.php
+++ src/CalendarDay.php
@@ -71,7 +71,7 @@ class CalendarDay implements CalendarDayInterface
         $this->events = $events;
     }
     
-    public function withEvents(array $events, int $openingHour, int $closingHour, int $timelineInterval, array $timeline) : self
+    public function withEvents(array $events, int $openingHour = 9, int $closingHour = 17, int $timelineInterval = 30, array $timeline = []) : self
     {
         $this->events = $events;
         $this->openingHour = $openingHour;
